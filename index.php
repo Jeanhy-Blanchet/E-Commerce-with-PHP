0b76070038253af3f4f11ef10a5eede6f0848434
Jeanhy-Blanchet <endiediejeanhy86@gmail.com>
2024-07-29 16:26:46 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
 <link rel="stylesheet" href="css/bootstrap.min.css" >
 <link rel="stylesheet" href="css/main.css" >
 <meta name="viewport" content="width=device=width,initial-scale=1,user-scaleable=o">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
-
-</script>
+<script src="js/jquery.min.js"></script>
 
 <script src="js/bootstrap.min.js"></script>
 </head>
